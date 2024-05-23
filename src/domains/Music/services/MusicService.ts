83e582b300f2cb36d938abafa2e6dfd08a749444
andreassisrg <andreassisrg@gmail.com>
2024-05-23 15:29:08 -0300

    feature: adição operações CRUD entidade Music

---
+++ src/domains/Music/services/MusicService.ts
@@ -0,0 +1,59 @@
+import { Music } from "@prisma/client";
+import prisma    from "../../../../config/prismaClient";
+
+class MusicService {
+    async create(body: Music) {
+        const music = await prisma.music.create({
+            data: {
+                name: body.name,
+                album: body.album,
+                genre: body.genre,
+                artistId: body.artistId
+            }
+        })
+
+        return music;
+    }
+
+    async readId(id: number) {
+        const music = await prisma.music.findUnique({
+            where: {
+                id: id
+            },
+        })
+
+        return music;
+    }
+
+    async readAll() {
+        const musics = await prisma.music.findMany({
+            orderBy: {
+                id: 'desc'
+            }
+        })
+        return musics;
+    }
+
+    async update(body: Music) {
+        const music = await prisma.music.update({
+            where: {
+                id: body.id
+            },
+            data: body
+        })
+        
+        return music;
+    }
+
+    async deleteId(id: number) {
+        const music = await prisma.music.delete({
+            where: {
+                id: id
+            },
+        })
+
+        return music;
+    }
+}
+
+export default new MusicService();
