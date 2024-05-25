5492cb3d37725657988f32d9c03a348c56537c7a
Isabela Schwindt <bebelaschwindt@gmail.com>
2024-05-25 08:13:52 -0300

    CRUD Artist

--- src/domains/Artist/services/ArtistService.ts
+++ src/domains/Artist/services/ArtistService.ts
@@ -13,6 +13,38 @@ class ArtistService {
         });
         return artist;
     }
+
+async readId(id: number){
+    const artist = await prisma.artist.findUnique ({
+        where: {
+            id: id
+        },
+    });
+    return artist;
+}
+
+async read(){
+    const artist = await prisma.artist.findMany ();
+    return artist;
+}
+
+async update(body: Artist){
+    const artist = await prisma.artist.update ({
+        where: {
+            id: body.id
+        },
+        data: body
+    });
+    return artist;
+}
+
+async deleteId(id:number){
+    const artist = await prisma.artist.delete({
+        where: {
+            id: id
+        },
+    });
+}
 }
 
 export default new ArtistService();
