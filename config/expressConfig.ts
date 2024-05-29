151cdfddd209b07baf6e8aeb6fb069bd18770c23
gabrielrolla <gabrielrolla@me.com>
2024-05-29 16:20:46 -0300

    feat: music_controllers

--- config/expressConfig.ts
+++ config/expressConfig.ts
@@ -1,18 +1,26 @@
 import cors, { CorsOptions } from "cors";
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import express, { Express } from "express";
+import MusicRouter from "../src/domains/Music/controllers/index";
 
 dotenv.config();
 
 export const app: Express = express();
 
 const options: CorsOptions = {
-    credentials: true,
-    origin: process.env.APP_URL
+	credentials: true,
+	origin: process.env.APP_URL
 };
 
 app.use(cors(options));
 app.use(express.json());
-app.use(express.urlcoded({
-    extend: true    
+app.use(express.urlencoded({
+	extended: true
 }));
+
+// Add controller routes...
+app.use("/api/musics", MusicRouter);
+
+
+app.listen(process.env.PORT);
+export default app;
