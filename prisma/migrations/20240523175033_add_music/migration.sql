bfb2a28a2ccaa1c5657b9be75f14e4e286ec568e
andreassisrg <andreassisrg@gmail.com>
2024-06-13 10:26:48 -0300

    feat: modificação do .schema para MySQL

--- prisma/migrations/20240523175033_add_music/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Music" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "genre" TEXT NOT NULL,
-    "Album" TEXT NOT NULL,
-    "ArtistId" INTEGER NOT NULL
-);
